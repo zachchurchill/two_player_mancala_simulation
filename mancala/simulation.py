523f000a24534c9f9f5061f7e5d4429f9b88219a
Zach Churchill <zachurchill@protonmail.com>
2021-03-03 00:19:10 -0500

    Add SimulationLoop class
    Provide testing around loop object and shore up edgecase of no player moves

--- mancala/simulation.py
+++ mancala/simulation.py
@@ -1,16 +1,34 @@
 import random
 from abc import ABCMeta, abstractmethod
+from typing import Dict, List, Optional
 
-from mancala.mancala import PlayerRow
+from mancala.mancala import (
+    Board,
+    Player,
+    PlayerRow,
+    Turn,
+    get_new_board,
+    take_turn,
+    who_gets_next_turn,
+)
 
 
 class PlayerStrategy(metaclass=ABCMeta):
+    @property
+    @abstractmethod
+    def strategy_name(self) -> str:
+        """Provide a unique name for the strategy."""
+
     @abstractmethod
     def choose_bin(self, player_row: PlayerRow) -> int:
         """Provides the player's bin selection for the Turn."""
 
 
 class ExampleRandomPlayerStrategy(PlayerStrategy):
+    @property
+    def strategy_name(self) -> str:
+        return "random-selection"
+
     def choose_bin(self, player_row: PlayerRow) -> int:
         nonempty_bins_with_index = [
             (i, b_i) for i, b_i in enumerate(player_row.bins) if b_i > 0
@@ -19,3 +37,129 @@ class ExampleRandomPlayerStrategy(PlayerStrategy):
             return random.choice(nonempty_bins_with_index)[0]
         else:
             raise ValueError("player_row does not contain any non-empty bins")
+
+
+class SimulationLoop:
+    def __init__(self, player_one: PlayerStrategy, player_two: PlayerStrategy):
+        self._strategies = {
+            Player.ONE: player_one,
+            Player.TWO: player_two,
+        }
+        self._reset_simulation()
+
+    def _reset_simulation(self) -> None:
+        self._has_run = False
+        self._starting_player = random.choice([Player.ONE, Player.TWO])
+        self._winning_player: Optional[Player] = None
+        self._turns: List[Turn] = []
+        self._boards = [get_new_board()]
+
+    @property
+    def player_one(self) -> Dict[Player, PlayerStrategy]:
+        return {Player.ONE: self._strategies[Player.ONE]}
+
+    @property
+    def player_two(self) -> Dict[Player, PlayerStrategy]:
+        return {Player.TWO: self._strategies[Player.TWO]}
+
+    @property
+    def starting_player(self) -> Dict[Player, PlayerStrategy]:
+        return {self._starting_player: self._strategies[self._starting_player]}
+
+    @property
+    def turns(self) -> List[Turn]:
+        return self._turns
+
+    @property
+    def boards(self) -> List[Board]:
+        return self._boards
+
+    @property
+    def has_run(self) -> bool:
+        return self._has_run
+
+    @property
+    def winning_player(self) -> Optional[Dict[Player, PlayerStrategy]]:
+        if self.has_run and self._winning_player is not None:
+            return {self._winning_player: self._strategies[self._winning_player]}
+        return None
+
+    def _is_end_of_game(self) -> bool:  # pragma: nocover
+        """In reality, the first player to get >24 pieces is the winner."""
+        latest_board = self.boards[-1]
+        return latest_board[Player.ONE].goal > 24 or latest_board[Player.TWO].goal > 24
+
+    def _set_winner(self) -> None:  # pragma: nocover
+        latest_board = self.boards[-1]
+        if latest_board[Player.ONE].goal > 24:
+            self._winning_player = Player.ONE
+        elif latest_board[Player.TWO].goal > 24:
+            self._winning_player = Player.TWO
+        else:
+            raise RuntimeError("winner has not been establish yet")
+
+    def run(self, reset_simulation=False) -> None:  # pragma: nocover
+        if self.has_run and not reset_simulation:
+            return
+        self._reset_simulation()
+
+        current_player = self._starting_player
+        current_turn = 0
+        while True:
+            # Set up objects for current player on this turn
+            current_board = self._boards[current_turn]
+            current_player_row = current_board[current_player]
+            current_player_strategy = self._strategies[current_player]
+
+            # Allow current player to select a bin from their row, and ensure it is valid
+            try:
+                selected_bin = current_player_strategy.choose_bin(current_player_row)
+            except ValueError:
+                # Assuming ValueError is thrown if player can't move, i.e. all bins are empty
+                if all([b == 0 for b in current_player_row.bins]):
+                    player_one_row = current_board[Player.ONE]
+                    player_two_row = current_board[Player.TWO]
+
+                    resulting_player_one_goal = (
+                        sum(player_one_row.bins) + player_one_row.goal
+                    )
+                    resulting_player_two_goal = (
+                        sum(player_two_row.bins) + player_two_row.goal
+                    )
+                    new_board = {
+                        Player.ONE: PlayerRow(
+                            bins=[0, 0, 0, 0, 0, 0], goal=resulting_player_one_goal
+                        ),
+                        Player.TWO: PlayerRow(
+                            bins=[0, 0, 0, 0, 0, 0], goal=resulting_player_two_goal
+                        ),
+                    }
+                    # self._turns.append(Turn.GameEndingTurn)  TODO
+                    self._boards.append(new_board)
+                    if resulting_player_one_goal > resulting_player_two_goal:
+                        self._winning_player = Player.ONE
+                    elif resulting_player_one_goal < resulting_player_two_goal:
+                        self._winning_player = Player.TWO
+                    else:
+                        self._winning_player = None  # tie
+                    self._has_run = True
+                    break
+
+            if current_player_row.bins[selected_bin] == 0:
+                raise ValueError(
+                    "Player strategies need to ensure they pick non-empty bins in 'choose_bin'"
+                )
+
+            # Perform turn with selected bin and save simulation data
+            turn = Turn(current_player, selected_bin)
+            new_board = take_turn(current_board, turn)
+            self._turns.append(turn)
+            self._boards.append(new_board)
+
+            # Check if game has ended; otherwise, update who is up next
+            if self._is_end_of_game():
+                self._set_winner()
+                self._has_run = True
+                break
+            current_player = who_gets_next_turn(current_board, turn, new_board)
+            current_turn += 1
--- mancala/tests/test_simulation.py
+++ mancala/tests/test_simulation.py
@@ -1,10 +1,15 @@
 import pytest
 
-from mancala.mancala import PlayerRow
+from mancala.mancala import Player, PlayerRow, get_new_board
 from mancala.simulation import (
     ExampleRandomPlayerStrategy,
     PlayerStrategy,
+    SimulationLoop,
 )
+
+
+def test_example_random_player_provides_strategy_name():
+    assert ExampleRandomPlayerStrategy().strategy_name is not None
 
 
 def test_example_random_player_strategy_chooses_valid_bins():
@@ -24,3 +29,69 @@ def test_example_random_player_strategy_raises_error_for_no_choices():
     random_strategy = ExampleRandomPlayerStrategy()
     with pytest.raises(ValueError):
         random_strategy.choose_bin(PlayerRow(bins=[0, 0, 0, 0, 0, 0], goal=10))
+
+
+def test_simulation_loops_provides_player_strategies_in_dict_format():
+    player_one = ExampleRandomPlayerStrategy()
+    player_two = ExampleRandomPlayerStrategy()
+    loop = SimulationLoop(player_one=player_one, player_two=player_two)
+    assert Player.ONE in loop.player_one
+    assert loop.player_one[Player.ONE] == player_one
+    assert Player.TWO in loop.player_two
+    assert loop.player_two[Player.TWO] == player_two
+
+
+def test_simulation_loop_sets_up_starting_player_upon_instantiation():
+    loop = SimulationLoop(
+        player_one=ExampleRandomPlayerStrategy(),
+        player_two=ExampleRandomPlayerStrategy(),
+    )
+    assert loop.starting_player is not None
+    assert loop.starting_player.keys() < {Player.ONE, Player.TWO}
+
+
+def test_simulation_loop_starts_off_with_no_run_and_winning_player():
+    loop = SimulationLoop(
+        player_one=ExampleRandomPlayerStrategy(),
+        player_two=ExampleRandomPlayerStrategy(),
+    )
+    assert loop.has_run is False
+    assert loop.winning_player is None
+
+
+def test_simulation_loop_starts_with_new_board():
+    loop = SimulationLoop(
+        player_one=ExampleRandomPlayerStrategy(),
+        player_two=ExampleRandomPlayerStrategy(),
+    )
+    assert len(loop.boards) == 1
+    assert loop.boards[0] == get_new_board()
+
+
+def test_simulation_loop_starts_with_no_turns():
+    loop = SimulationLoop(
+        player_one=ExampleRandomPlayerStrategy(),
+        player_two=ExampleRandomPlayerStrategy(),
+    )
+    assert len(loop.turns) == 0
+
+
+def test_simulation_loop_stops_when_goal_is_over_24():
+    player_one = ExampleRandomPlayerStrategy()
+    player_two = ExampleRandomPlayerStrategy()
+    loop = SimulationLoop(player_one=player_one, player_two=player_two)
+    assert loop.has_run is False
+    assert loop.winning_player is None
+
+    loop.run()
+    assert loop.has_run is True
+    assert len(loop.turns) > 1
+    assert len(loop.boards) > 1
+
+    if loop.winning_player:  # pragma: nocover
+        winning_player, *_ = loop.winning_player.keys()
+        assert loop.boards[-1][winning_player].goal >= 24
+    else:  # pragma: nocover
+        last_board = loop.boards[-1]
+        assert last_board[Player.ONE].goal == 24
+        assert last_board[Player.TWO].goal == 24
