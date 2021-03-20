7cec742622dfac41749ea7588854eb74fefea160
Zach Churchill <zachurchill@protonmail.com>
2021-03-19 22:44:10 -0400

    Add player strategy for always choosing bin with min pieces

--- mancala/strategy.py
+++ mancala/strategy.py
@@ -28,3 +28,19 @@ class ExampleRandomPlayerStrategy(PlayerStrategy):
             return random.choice(nonempty_bins_with_index)[0]
         else:
             raise ValueError("player_row does not contain any non-empty bins")
+
+
+class AlwaysMinimumPlayerStrategy(PlayerStrategy):
+    @property
+    def strategy_name(self) -> str:
+        return "always-minimum"
+
+    def choose_bin(self, player_row: PlayerRow) -> int:
+        nonempty_bins_with_index = [
+            (i, b_i) for i, b_i in enumerate(player_row.bins) if b_i > 0
+        ]
+        # Return first index with the minimal amount of pieces in bin
+        if len(nonempty_bins_with_index) > 0:
+            return min(nonempty_bins_with_index, key=lambda item: item[1])[0]
+        else:
+            raise ValueError("player_row does not contain any non-empty-bins")
--- mancala/tests/test_strategy.py
+++ mancala/tests/test_strategy.py
@@ -1,7 +1,10 @@
 import pytest
 
 from mancala.mancala import PlayerRow
-from mancala.strategy import ExampleRandomPlayerStrategy
+from mancala.strategy import (
+    AlwaysMinimumPlayerStrategy,
+    ExampleRandomPlayerStrategy,
+)
 
 
 def test_example_random_player_provides_strategy_name():
@@ -25,3 +28,29 @@ def test_example_random_player_strategy_raises_error_for_no_choices():
     random_strategy = ExampleRandomPlayerStrategy()
     with pytest.raises(ValueError):
         random_strategy.choose_bin(PlayerRow(bins=[0, 0, 0, 0, 0, 0], goal=10))
+
+
+def test_always_minimum_player_provides_strategy_name():
+    assert AlwaysMinimumPlayerStrategy().strategy_name is not None
+
+
+def test_always_minimum_player_strategy_chooses_bin_with_least_pieces():
+    always_min_strategy = AlwaysMinimumPlayerStrategy()
+
+    for i in range(6):
+        one_nonempty_bin_row = PlayerRow(
+            bins=[1 if i == j else 0 for j in range(6)], goal=20
+        )
+        assert always_min_strategy.choose_bin(one_nonempty_bin_row) == i
+
+    all_ones_row = PlayerRow(bins=[1, 1, 1, 1, 1, 1], goal=15)
+    assert always_min_strategy.choose_bin(all_ones_row) == 0
+
+    two_ones_at_end_of_row = PlayerRow(bins=[2, 0, 4, 3, 1, 1], goal=12)
+    assert always_min_strategy.choose_bin(two_ones_at_end_of_row) == 4
+
+
+def test_always_minimum_player_strategy_raises_error_for_no_choices():
+    always_min_strategy = AlwaysMinimumPlayerStrategy()
+    with pytest.raises(ValueError):
+        always_min_strategy.choose_bin(PlayerRow(bins=[0, 0, 0, 0, 0, 0], goal=12))
