b11a337ab3e9d88747509ac054a4a515e3540949
Zach Churchill <zachurchill@protonmail.com>
2021-03-21 09:54:29 -0400

    Update helper script to choose starting player

--- run_simulations.py
+++ run_simulations.py
@@ -1,6 +1,8 @@
 import json
 from pprint import pprint
+from typing import Optional
 
+from mancala.mancala import Player
 from mancala.simulation import SimulationLoop
 from mancala.strategy import (
     AlwaysMaximumPlayerStrategy,
@@ -10,55 +12,66 @@ from mancala.strategy import (
 )
 
 
-def min_vs_min():
+def min_vs_min(starting_player: Optional[Player] = None):
     loop = SimulationLoop(
         player_one=AlwaysMinimumPlayerStrategy(),
         player_two=AlwaysMinimumPlayerStrategy(),
+        starting_player=starting_player,
     )
-    pprint(loop.starting_player)
+    print(f"Starting player: {loop.starting_player}")
     loop.run()
     pprint(json.loads(loop.serialize()))
+    print()
 
 
-def min_vs_max():
+def min_vs_max(starting_player: Optional[Player] = None):
     loop = SimulationLoop(
         player_one=AlwaysMaximumPlayerStrategy(),
         player_two=AlwaysMinimumPlayerStrategy(),
+        starting_player=starting_player,
     )
-    pprint(loop.starting_player)
+    print(f"Starting player: {loop.starting_player}")
     loop.run()
     pprint(json.loads(loop.serialize()))
+    print()
 
 
-def max_vs_max():
+def max_vs_max(starting_player: Optional[Player] = None):
     loop = SimulationLoop(
         player_one=AlwaysMaximumPlayerStrategy(),
         player_two=AlwaysMaximumPlayerStrategy(),
+        starting_player=starting_player,
     )
-    pprint(loop.starting_player)
+    print(f"Starting player: {loop.starting_player}")
     loop.run()
     pprint(json.loads(loop.serialize()))
+    print()
 
 
-def min_vs_even_goal():
+def min_vs_even_goal(starting_player: Optional[Player] = None):
     loop = SimulationLoop(
         player_one=AlwaysMinimumPlayerStrategy(),
         player_two=EvenGoalOrPiecesOnOtherSideStrategy(),
+        starting_player=starting_player,
     )
-    pprint(loop.starting_player)
+    print(f"Starting player: {loop.starting_player}")
     loop.run()
     pprint(json.loads(loop.serialize()))
+    print()
 
 
-def max_vs_even_goal():
+def max_vs_even_goal(starting_player: Optional[Player] = None):
     loop = SimulationLoop(
         player_one=AlwaysMaximumPlayerStrategy(),
         player_two=EvenGoalOrPiecesOnOtherSideStrategy(),
+        starting_player=starting_player,
     )
-    pprint(loop.starting_player)
+    print(f"Starting player: {loop.starting_player}")
     loop.run()
     pprint(json.loads(loop.serialize()))
+    print()
 
 
 if __name__ == "__main__":
-    min_vs_even_goal()
+    min_vs_even_goal(Player.ONE)
+    min_vs_even_goal(Player.TWO)
