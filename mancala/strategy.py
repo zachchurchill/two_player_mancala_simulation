356e048b8b9426e492c58f67937ec200067ac5a9
Zach Churchill <zachurchill@protonmail.com>
2021-03-20 21:24:23 -0400

    Create new strategy and refactor strategy tests

--- mancala/strategy.py
+++ mancala/strategy.py
@@ -60,3 +60,41 @@ class AlwaysMaximumPlayerStrategy(PlayerStrategy):
             return max(nonempty_bins_with_index, key=lambda item: item[1])[0]
         else:
             raise ValueError("player_row does not contain any non-empty-bins")
+
+
+class EvenGoalOrPiecesOnOtherSideStrategy(PlayerStrategy):
+    """Custom strategy focused on goal making or shedding pieces.
+
+    This strategy will first try to see if there are any bins that
+    contain the correct amount of pieces to make a goal, where the
+    first bin to contain such an amount of pieces is chosen. For
+    example, if the row is [  5 |  4,  0,  3,  8,  1,  0 ], then
+    the strategy would choose bin 2 (zero-indexed) so that a goal is
+    scored. Now, if no such bin exists, then the strategy will look
+    for the bin that tries to move the most pieces to the opponent's
+    row. Assuming the following example above first chose bin 2 to
+    score a goal and get another turn, [  6 |  5,  1,  0,  8,  1,  0 ],
+    the strategy would then choose bin 0 because although bin 3 would
+    put the same number of pieces on the opponent's side (4), the
+    strategy chooses the first such bin.
+
+    """
+
+    @property
+    def strategy_name(self) -> str:
+        return "even-goal-or-more-pieces-to-opponent"
+
+    def choose_bin(self, player_row: PlayerRow) -> int:
+        if all(b_i == 0 for b_i in player_row.bins):
+            raise ValueError("player_row does not contain any non-empty-bins")
+
+        goal_making_bins = [
+            i for i, b_i in enumerate(player_row.bins) if b_i > 0 and b_i == i + 1
+        ]
+        if len(goal_making_bins) > 0:
+            return goal_making_bins[0]
+
+        number_of_pieces_in_opponents_row = [
+            (i, b_i - i - 1) for i, b_i in enumerate(player_row.bins) if b_i > 0
+        ]
+        return max(number_of_pieces_in_opponents_row, key=lambda item: item[1])[0]
--- mancala/tests/test_strategy.py
+++ mancala/tests/test_strategy.py
@@ -1,15 +1,35 @@
+from typing import List
+
 import pytest
 
 from mancala.mancala import PlayerRow
 from mancala.strategy import (
     AlwaysMaximumPlayerStrategy,
     AlwaysMinimumPlayerStrategy,
+    EvenGoalOrPiecesOnOtherSideStrategy,
     ExampleRandomPlayerStrategy,
+    PlayerStrategy,
 )
 
 
-def test_example_random_player_provides_strategy_name():
-    assert ExampleRandomPlayerStrategy().strategy_name is not None
+def _get_all_strategies() -> List[PlayerStrategy]:
+    return [
+        AlwaysMaximumPlayerStrategy(),
+        AlwaysMinimumPlayerStrategy(),
+        EvenGoalOrPiecesOnOtherSideStrategy(),
+        ExampleRandomPlayerStrategy(),
+    ]
+
+
+@pytest.mark.parametrize("strategy", _get_all_strategies())
+def test_player_strategy_provides_strategy_name(strategy):
+    assert strategy.strategy_name is not None
+
+
+@pytest.mark.parametrize("strategy", _get_all_strategies())
+def test_player_strategy_raises_error_for_no_choices(strategy):
+    with pytest.raises(ValueError):
+        strategy.choose_bin(PlayerRow(bins=[0, 0, 0, 0, 0, 0], goal=10))
 
 
 def test_example_random_player_strategy_chooses_valid_bins():
@@ -23,16 +43,6 @@ def test_example_random_player_strategy_chooses_valid_bins():
 
     semi_empty_bin_row = PlayerRow(bins=[1, 0, 1, 0, 1, 0], goal=15)
     assert random_strategy.choose_bin(semi_empty_bin_row) in [0, 2, 4]
-
-
-def test_example_random_player_strategy_raises_error_for_no_choices():
-    random_strategy = ExampleRandomPlayerStrategy()
-    with pytest.raises(ValueError):
-        random_strategy.choose_bin(PlayerRow(bins=[0, 0, 0, 0, 0, 0], goal=10))
-
-
-def test_always_minimum_player_provides_strategy_name():
-    assert AlwaysMinimumPlayerStrategy().strategy_name is not None
 
 
 def test_always_minimum_player_strategy_chooses_bin_with_least_pieces():
@@ -51,16 +61,6 @@ def test_always_minimum_player_strategy_chooses_bin_with_least_pieces():
     assert always_min_strategy.choose_bin(two_ones_at_end_of_row) == 4
 
 
-def test_always_minimum_player_strategy_raises_error_for_no_choices():
-    always_min_strategy = AlwaysMinimumPlayerStrategy()
-    with pytest.raises(ValueError):
-        always_min_strategy.choose_bin(PlayerRow(bins=[0, 0, 0, 0, 0, 0], goal=12))
-
-
-def test_always_maximum_player_provides_strategy_name():
-    assert AlwaysMaximumPlayerStrategy().strategy_name is not None
-
-
 def test_always_maximum_player_strategy_chooses_bin_with_least_pieces():
     always_max_strategy = AlwaysMaximumPlayerStrategy()
 
@@ -77,7 +77,35 @@ def test_always_maximum_player_strategy_chooses_bin_with_least_pieces():
     assert always_max_strategy.choose_bin(two_sixes_at_end_of_row) == 4
 
 
-def test_always_maximum_player_strategy_raises_error_for_no_choices():
-    always_max_strategy = AlwaysMaximumPlayerStrategy()
-    with pytest.raises(ValueError):
-        always_max_strategy.choose_bin(PlayerRow(bins=[0, 0, 0, 0, 0, 0], goal=12))
+def test_even_goal_strategy_chooses_bin_with_goal_making_moves():
+    even_goal_strategy = EvenGoalOrPiecesOnOtherSideStrategy()
+
+    for i in range(6):
+        chosen_bin = even_goal_strategy.choose_bin(
+            PlayerRow(bins=[(i + 1) if i == j else 0 for j in range(6)], goal=0)
+        )
+        assert chosen_bin == i
+
+    documentation_example_row = PlayerRow(bins=[4, 0, 3, 8, 1, 0], goal=5)
+    assert even_goal_strategy.choose_bin(documentation_example_row) == 2
+
+
+@pytest.mark.parametrize(
+    "bins,expected_selection",
+    [
+        ([5, 1, 0, 8, 1, 0], 0),
+        ([4, 1, 0, 8, 1, 0], 3),
+        ([0, 0, 1, 0, 2, 1], 2),
+        ([0, 0, 1, 2, 0, 1], 2),
+        ([0, 0, 1, 3, 0, 1], 3),
+        ([0, 0, 0, 0, 0, 1], 5),
+    ],
+)
+def test_even_goal_strategy_chooses_bin_with_max_opponent_pieces(
+    bins, expected_selection
+):
+    even_goal_strategy = EvenGoalOrPiecesOnOtherSideStrategy()
+    assert (
+        even_goal_strategy.choose_bin(PlayerRow(bins=bins, goal=0))
+        == expected_selection
+    )
